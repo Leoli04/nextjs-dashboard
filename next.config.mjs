bdfb96f35e8c2b4a0efd642a02e89d66661943d3
LeoLi04 <leoli_4@163.com>
2024-09-19 14:23:11 +0800

    feat: 数据部分

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,10 @@
 /** @type {import('next').NextConfig} */
 
-const nextConfig = {};
+const nextConfig = {
+    experimental: {
+        // “incremental”值允许您为特定路由采用 PPR。
+        ppr: 'incremental',
+      },
+};
 
 export default nextConfig;
